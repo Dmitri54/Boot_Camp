5dc90fd461f490dedf65a4528b38e50976518714
Dmitri5 <dmitriprogram@gmail.com>
2022-08-20 20:13:49 +0700

    Added lesson 09 Server

---
+++ lesson_09_Server/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Урок 9. Клиент-серверное взаимодействие. 
+Тут будет описан класс Server */
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Это наш сервер");
+            OurServer server = new OurServer();
+        }
+    }
+
+}
