312d080635a0c00f6ebbb7973240a83a6ce8c249
Dmitri5 <dmitriprogram@gmail.com>
2022-08-20 20:41:47 +0700

    Added lesson 09 Server with the ability to respond to the client

--- lesson_09_Client/OurClient.cs
+++ lesson_09_Client/OurClient.cs
@@ -8,10 +8,12 @@ namespace Client
     {
         private TcpClient client; // создал переменную при помощи, которой я могу работать с TSP. От клиента к серверу. Посути это и есть мой клиент.
         private StreamWriter sWriter; // Импровезированный поток, при помощи, которого я быду отправлять с клиента на сервер.
+        private StreamReader sReader; // Создаю ещё один поток, чтобы получать сообщения от сервера.
         public OurClient()
         {
             client = new TcpClient("127.0.0.1", 5555);
-            sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
+            sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8); // Чтение
+            sReader = new StreamReader(client.GetStream(), Encoding.UTF8); // Запись
 
             HandleComunicanion(); // Как только я создал клиента, нужно сразу это соедеинение держать.
         }
@@ -24,6 +26,9 @@ namespace Client
                 string? message = Console.ReadLine();
                 sWriter.WriteLine(message);
                 sWriter.Flush(); // Принудительно отправит сообщение сразу, после его создания.
+                
+                string answerServer = sReader.ReadLine();
+                Console.WriteLine($"Сервер ответил -> {answerServer}");
             }
         }
     }
